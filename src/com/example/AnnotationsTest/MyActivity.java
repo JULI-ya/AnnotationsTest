9a6e0d6ef3c66f2cc45e84b2f1bc61e67bf81d11
y.klimko <y.klimko@itransition.com>
2013-11-05 16:16:38 +0300

    Parse example

--- src/com/example/AnnotationsTest/MyActivity.java
+++ src/com/example/AnnotationsTest/MyActivity.java
@@ -2,21 +2,34 @@ package com.example.AnnotationsTest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import com.example.AnnotationsProcessor.CustomAnnotation;
-import com.example.AnnotationsProcessor.Handleable;
-import com.example.AnnotationsProcessor.Option;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+@CustomAnnotation(className = "String", type = 1)
 public class MyActivity extends Activity {
 
-    @Handleable
-    private String stuff;
+    private String stuff = "{\n" +
+            "    \"id\": 12,\n" +
+            "    \"name\": \"Yuliya\",\n" +
+            "    \"isCheck\": true\n" +
+            "}";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        MyObj myObj = new MyObj();
+        try {
+            MyObj.parse(new JSONObject(stuff), myObj);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d(getPackageName(), " " + myObj.name);
     }
 
-    @CustomAnnotation(className = "String", type = 1)
+    @CustomAnnotation(className = "String", type = 2)
     public void annotatedMethod(String value) {
-    }
+}
 }
