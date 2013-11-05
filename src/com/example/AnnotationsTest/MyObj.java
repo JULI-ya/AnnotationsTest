dd38af10746c2e7f01e81d8ef79fcf24f39f9e90
y.klimko <y.klimko@itransition.com>
2013-11-05 16:18:53 +0300

    Parse example

---
+++ src/com/example/AnnotationsTest/MyObj.java
@@ -0,0 +1,15 @@
+package com.example.AnnotationsTest;
+
+
+import com.example.AnnotationsProcessor.ParcelableObj;
+
+@ParcelableObj
+public class MyObj extends MyObjProjection  {
+
+    public int id;
+    public String name;
+    boolean isCheck;
+
+
+}
+
